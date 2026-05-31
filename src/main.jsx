02d2aa5d9e36b747d6882c6b5be18d189baae6e8
jai <hashiramareign@gmail.com>
2026-06-01 04:23:58 +0800

    deleted landing page

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import Landing from './Landing.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/app" element={<App />} />
-      </Routes>
-    </HashRouter>
+    <App />
   </StrictMode>
 )
